10d0302d188e1fdbc006a13e0b438ebf052a05da
MNK5490 <maxwell.n.kendall@gmail.com>
2016-08-20 11:03:28 -0400

    JS Wired Sufficiently
    Could still add “data start/end” attributes and wire the exceeds
    requirements. For now, I will be styling the document to look like the
    mockup and come back to the JS later.

--- js/main.js
+++ js/main.js
@@ -6,56 +6,65 @@
                 play: document.getElementById("play"),
                 volume: document.getElementById("volume"),
                 fScreen: document.getElementById("fscreen"),
+                currentTime: document.getElementById("currentTime")
             };
 
             var transcript = {
-                element1: document.getElementsByTagName("p")[0].innerHTML,
-                element2: document.getElementsByTagName("p")[1].innerHTML,
-                isPlaying: false,
-                property : function () {
-
-                },
+                element1: [
+                        document.getElementsByTagName("p")[0].innerHTML.substr(0, 500),
+                        document.getElementsByTagName("p")[0].innerHTML.substr(500, 1000)
+                ],
+                element2: document.getElementsByTagName("p")[1],
+                isPlaying: false
             };
 
             var video = {
                 element: document.getElementsByTagName("video")[0],
-                // currentTime: document.getElementsByTagName("video")[0].currentTime,
-                //^^always returns a 0 in the console
+                progress: document.getElementById("videotrack"),
+                controls: document.getElementById("controls"),
                 muted: false,
-                fullScreen: false
+                fullScreen: false,
+                isPlaying: false
+
             };
 
 // b. FUNCTIONS:
 
             // Progress Bar Increase
 
-                function progressBar(){
+            function setDuration() {
+                var progress = document.getElementById("progressBar");
+               progress.setAttribute('max', video.duration);
+           }
+           function progressIncrease(){
+               var progressBar = document.getElementById("progressBar");
+               progressBar.value = video.element.currentTime;
+               progressBar.style.width = Math.floor((video.element.currentTime / video.element.duration) * 100) + '%';
+            };
 
-                    console.log(video.element.currentTime);
-                    // this function is called everytime the time updates
-                        // properites for currentTime, totalTime
-                            // divide these two = duration remaining
-                                //progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
-                        // each time, we want to select a element -- namely, the progress bar -- and increase its width by percentage,
-                        // allow the user to set the time of the video, "click" event handler on the progressbar
-                        //https://developer.mozilla.org/en-US/Apps/Fundamentals/Audio_and_video_delivery/cross_browser_video_player
-                }
+        // Seek function
+
+        function seek(e) {
+           var pos = (e.pageX  - this.offsetLeft) / this.offsetWidth;
+           video.element.currentTime = pos * video.element.duration;
+       };
 
             // Play Button
 
             function playVideo(){
-                if(video.isPlaying){
+                if(video.isPlaying === false){
+                    video.element.play();
+                    buttons.play.style.background = 'url(icons/pause-icon.png) black no-repeat center';
+                    video.isPlaying = true;
+                } else {
                     video.isPlaying = false;
                     video.element.pause();
                     buttons.play.style.background = 'url(icons/play-icon.png) black no-repeat center';
-                } else {
-                video.element.play();
-                buttons.play.style.background = 'url(icons/pause-icon.png) black no-repeat center';
-                video.isPlaying = true;
                     }
             }
 
             //  Volume On & Off
+
             function volumeOff(){
                 var vid = video.element;
                 var volumeButton = buttons.volume;
@@ -71,31 +80,66 @@
                     }
             }
 
-            //  Full Screen Request
-
-            function fullScreen(){
-                var vid = video.element;
-                console.log(vid);
-                if (vid.mozRequestFullScreen) {
-                     // This is how to go into fullscren mode in Firefox
-                     // Note the "moz" prefix, which is short for Mozilla.
-                     vid.mozRequestFullScreen();
-                 } else if (vid.webkitRequestFullScreen) {
-                     // This is how to go into fullscreen mode in Chrome and Safari
-                     // Both of those browsers are based on the Webkit project, hence the same prefix.
-                     vid.webkitRequestFullScreen();
-                  }
-                  // Hooray, now we're in fullscreen mode!
-                 }
-
              // Highlight Transcript Function
 
-                 function highlightTranscript (rangeOfTimes, subStrOfParagraph){
-                     if(video.currentTime === rangeOfTimes){
-                         subStrOfParagraph.style.color = "white";
+                 function highlightTranscript (){
+                     var a = document.getElementById("1");
+                     var b = document.getElementById("2");
+                     var c = document.getElementById("3");
+                     var d = document.getElementById("4");
+                     var e = document.getElementById("5");
+                     var f = document.getElementById("6");
+                     var g = document.getElementById("7");
+                     var h = document.getElementById("8");
+                     var z = document.getElementById("9");
+                     var j = document.getElementById("10");
+                     var k = document.getElementById("11");
+                     var l = document.getElementById("12");
+                     for (var i = 0; i < document.getElementsByTagName("span").length; i ++){
+                         document.getElementsByTagName("span")[i].style.color = "black";
+                     }
+                     if(video.element.currentTime < 5){
+                         a.style.color = "orange";
+                     } else if (video.element.currentTime > 5 && video.element.currentTime < 10){
+                                                  b.style.color = "orange";
+                     } else if (video.element.currentTime > 10 && video.element.currentTime < 15){
+                                                  c.style.color = "orange";
+                     } else if (video.element.currentTime > 15 && video.element.currentTime < 20){
+                                                  d.style.color = "orange";
+                     } else if (video.element.currentTime > 20 && video.element.currentTime < 25){
+                                                  e.style.color = "orange";
+                     } else if (video.element.currentTime > 25 && video.element.currentTime < 30){
+                                                  f.style.color = "orange";
+                     } else if (video.element.currentTime > 30 && video.element.currentTime < 35){
+                                                  g.style.color = "orange";
+                     } else if (video.element.currentTime > 35 && video.element.currentTime < 40){
+                                                  h.style.color = "orange";
+                     } else if (video.element.currentTime > 40 && video.element.currentTime < 45){
+                                                  z.style.color = "orange";
+                     } else if (video.element.currentTime > 45 && video.element.currentTime < 50){
+                                                  j.style.color = "orange";
+                     } else if (video.element.currentTime > 50 && video.element.currentTime < 55){
+                         k.style.color = "orange";
+                     } else if (video.element.currentTime > 55 && video.element.currentTime < 60){
+                         l.style.color = "orange";
                      }
                  }
-                    // add styling when isPlaying is true
+
+            //  Full Screen Request
+
+                    function fullScreen(){
+                        var vid = video.element;
+                        if (vid.mozRequestFullScreen) {
+                             // This is how to go into fullscren mode in Firefox
+                             // Note the "moz" prefix, which is short for Mozilla.
+                             vid.mozRequestFullScreen();
+                         } else if (vid.webkitRequestFullScreen) {
+                             // This is how to go into fullscreen mode in Chrome and Safari
+                             // Both of those browsers are based on the Webkit project, hence the same prefix.
+                             vid.webkitRequestFullScreen();
+                          }
+                          // Hooray, now we're in fullscreen mode!
+                         }
 
             // Hiding and Showing Video on Hover
 
@@ -108,6 +152,27 @@
                         controls.style.visibility = "hidden";
                     };
 
+        // Ended Video function
+
+            function videoOver(){
+                console.log('done');
+                video.element.currentTime = 0;
+                video.isPlaying = false;
+                buttons.play.style.background = 'url(icons/play-icon.png) black no-repeat center';
+            }
+
+            function displayCurrentTime(){
+                var x = video.element.currentTime;
+                if(x < 10){
+                buttons.currentTime.innerHTML = "00:0" + parseInt(x);
+            }   else if (x >= 10){
+                buttons.currentTime.innerHTML = "00:" + parseInt(x);
+            } else if (x >= 60 && x < 70){
+                buttons.currentTime.innerHTML = "01:0" + parseInt(x);
+            } else if (x >= 70){
+                buttons.currentTime.innerHTML = "01:" + parseInt(x);
+            }
+            }
 
 // c. EVENT LISTENERS:
 
@@ -116,7 +181,13 @@
             buttons.volume.addEventListener("click", volumeOff, false)
             buttons.fScreen.addEventListener("click", fullScreen, false)
     // Video Events
-            video.element.addEventListener("timeupdate", progressBar, false);
+            video.element.addEventListener("timeupdate", progressIncrease, false);
+            video.element.addEventListener("timeupdate", highlightTranscript, false);
+            video.element.addEventListener("ended", videoOver, false);
+            video.element.addEventListener("loadmetadata", setDuration, false);
             video.element.addEventListener("mouseover", hoverOn, false);
-            document.getElementById("controls").addEventListener("mouseover", hoverOn, false);
             video.element.addEventListener("mouseleave", hoverOff, false);
+            video.controls.addEventListener("mouseover", hoverOn, false);
+            video.controls.addEventListener("mouseleave", hoverOff, false);
+            video.progress.addEventListener('click', seek, false);
+            video.element.addEventListener('timeupdate', displayCurrentTime, false);
